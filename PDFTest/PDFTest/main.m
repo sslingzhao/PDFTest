07da4431ad433aaab91933fa64c89d4053315432
ShineLing <987410331@qq.com>
2017-04-26 11:58:03 +0800

    初始化demo
    初始化demo

---
+++ PDFTest/PDFTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PDFTest
+//
+//  Created by mac on 2017/4/26.
+//  Copyright © 2017年 ShineLing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
